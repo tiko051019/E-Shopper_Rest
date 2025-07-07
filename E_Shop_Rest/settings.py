e763429443b3a4907328cd4211efc06bc2335462
Tigran <tikoabrahamyan05@gmail.com>
2025-07-07 21:56:10 +0400

    Update settings.py

--- E_Shop_Rest/settings.py
+++ E_Shop_Rest/settings.py
@@ -148,7 +148,7 @@ REST_FRAMEWORK = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'tikoabrahamyan05@gmail.com'
-EMAIL_HOST_PASSWORD = 'xnnkorsmhhlxuraw'
+EMAIL_HOST_PASSWORD = '.................'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BECKEND = 'django.core.mail.backends.smtp.EmailBackend'
